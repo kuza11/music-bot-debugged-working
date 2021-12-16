2cd1e636efa33a5da359c037588dccff2c2a5381
kuza11 <kuba.zvolanek@seznam.cz>
2021-12-16 21:37:05 +0100

    working

---
+++ Music-bot/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: 'k!',
+        token: 'OTE4ODc4ODkxNDY3MjQzNTkw.YbNqpw.7lmAC0bL4nZkocnM3-PQKvOY1eY',
+        playing: 'k!help | debugged (zatím úplně)',
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
---
+++ Music-bot/src/events.js
@@ -0,0 +1,42 @@
+var server, chanl;
+player.on('voiceStateUpate', (oldState, newState) =>{
+    server = oldState.guild.id;
+    chanl = oldState.channel;
+});
+
+player.on('error', (queue, error) => {
+    console.log(`Error emitted from the queue ${error.message}`);
+    var chan = queue.connection.channel;
+    //queue.connection.end();
+    queue.destroy();
+    queue.connect(chan);
+});
+
+player.on('connectionError', (queue, error) => {
+    console.log(`Error emitted from the connection ${error.message}`);
+    var chan = queue.connection.channel;
+    //queue.connection.end();
+    queue.destroy();
+    queue.connect(chan);
+});
+
+player.on('trackStart', (queue, track) => {
+    if (!client.config.opt.loopMessage && queue.repeatMode !== 0) return;
+    queue.metadata.send(`Started playing ${track.title} in **${queue.connection.channel.name}** 🎧`);
+});
+
+player.on('trackAdd', (queue, track) => {
+    queue.metadata.send(`Track ${track.title} added in the queue ✅`);
+});
+
+player.on('botDisconnect', (queue) => {
+    queue.metadata.send('I was manually disconnected from the voice channel, clearing queue... ❌');
+});
+
+player.on('channelEmpty', (queue) => {
+    queue.metadata.send('Nobody is in the voice channel, leaving the voice channel... ❌');
+});
+
+player.on('queueEnd', (queue) => {
+    queue.metadata.send('I finished reading the whole queue ✅');
+});
